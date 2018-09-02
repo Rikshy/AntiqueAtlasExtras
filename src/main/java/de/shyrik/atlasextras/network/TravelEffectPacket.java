018a6361ea6c18855922f9d6e7c65bcb63f8da71
Shyrik <r.wesenberg@gmail.com>
2018-09-02 22:58:47 +0200

    fixed server crash on start up

--- src/main/java/de/shyrik/atlasextras/network/TravelEffectPacket.java
+++ src/main/java/de/shyrik/atlasextras/network/TravelEffectPacket.java
@@ -10,6 +10,8 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class TravelEffectPacket implements IMessage, IMessageHandler<TravelEffectPacket, IMessage> {
 
@@ -33,6 +35,7 @@ public class TravelEffectPacket implements IMessage, IMessageHandler<TravelEffec
     }
 
     @Override
+    @SideOnly(Side.CLIENT)
     public IMessage onMessage(TravelEffectPacket message, MessageContext ctx) {
         NetworkHelper.getThreadListener(ctx).addScheduledTask(() -> {
             Minecraft mc = Minecraft.getMinecraft();
