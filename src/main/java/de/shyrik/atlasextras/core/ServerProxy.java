d9ff8473218baa0242093ca6d9ecf369433df1d0
Shyrik <r.wesenberg@gmail.com>
2018-09-14 19:08:29 +0200

    ...

--- src/main/java/de/shyrik/atlasextras/core/ServerProxy.java
+++ src/main/java/de/shyrik/atlasextras/core/ServerProxy.java
@@ -1,17 +1,7 @@
 package de.shyrik.atlasextras.core;
 
-import de.shyrik.atlasextras.AtlasExtras;
-import de.shyrik.atlasextras.compat.WaystonesHandler;
-import de.shyrik.atlasextras.features.travel.AtlasExtrasCostHandler;
-import de.shyrik.atlasextras.compat.SignpostHandler;
-import de.shyrik.atlasextras.features.travel.TravelHandler;
-import net.blay09.mods.waystones.network.NetworkHandler;
-import net.blay09.mods.waystones.network.message.MessageEditWaystone;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.relauncher.Side;
 
 public class ServerProxy implements IModProxy {
 
